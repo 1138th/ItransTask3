94c3bac04c6d5c30120e6d30b33477847f3c8bc9
Ansar D. <dakuansar7@gmail.com>
2024-04-16 23:28:18 +0500

    remove debug files, remove debug code

--- ITransitionTask3/Program.cs
+++ ITransitionTask3/Program.cs
@@ -4,18 +4,16 @@ public class Program : Game
 {
     public static void Main(string[] args)
     {
-        string[] tempArgs = ["rock", "scissors", "paper", "lizard", "spock"];
-
-        if (tempArgs.Length < 3)
+        if (args.Length < 3)
             Console.WriteLine("The amount of arguments should be more than 2.");
-        else if (tempArgs.Length % 2 != 1)
+        else if (args.Length % 2 != 1)
             Console.WriteLine("The amount of arguments is incorrect. Expected: odd (3, 5, 7 etc)");
         else
         {
             var allTurns = new Dictionary<int, string>();
-            for (var i = 1; i <= tempArgs.Length; i++)
+            for (var i = 1; i <= args.Length; i++)
             {
-                allTurns.Add(i, tempArgs[i - 1]);
+                allTurns.Add(i, args[i - 1]);
             }
         
             AllTurns = allTurns;
